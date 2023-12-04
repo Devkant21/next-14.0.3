d6b8c4096e55b4ceab82c968bd0c66be3b859bf1
Devkant <devkant21@gmail.com>
2023-12-04 23:03:18 +0530

    🚀 : Next 14.0.3 from scratch

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header"
+
+export default function Page() {
+    return (
+        <section>
+            <Header>
+                <h1>Header</h1>
+            </Header>
+            <p>This is an amaing</p>
+        </section>
+    )
+}
---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import {Header} from "../components/Header";
+
+export default function About() {
+    return (
+        <>
+            <Header>
+                <h1>Header</h1>
+            </Header>
+            <h1>About</h1>
+        </>
+
+    )
+}
